d3daeb0b2022170c1f96816e3210ef316bf82bc3
Syed Suhel M <syedsuhel2624@gmail.com>
2023-02-21 12:31:25 +0530

    removed drawer

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { IconComponentProvider } from '@react-native-material/core';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
@@ -7,18 +7,18 @@ import { DraftsStack } from './routes/DraftsStack';
 import { InboxStack } from './routes/InboxStack';
 import Inbox from './screens/Inbox';
 
-const Drawer= createDrawerNavigator()
+// const Drawer= createDrawerNavigator()
 
 export default function App() {
   return (
     <IconComponentProvider>
       <StatusBar style="auto" />
       <NavigationContainer>
-        {/* <InboxStack/> */}
-        <Drawer.Navigator screenOptions={{headerShown:false}}>
+        <InboxStack/>
+        {/* <Drawer.Navigator screenOptions={{headerShown:false}}>
           <Drawer.Screen name='Inbox' component={InboxStack}/>
           <Drawer.Screen name='Drafts' component={DraftsStack}/>
-        </Drawer.Navigator>
+        </Drawer.Navigator> */}
 
       </NavigationContainer>
     </IconComponentProvider>
