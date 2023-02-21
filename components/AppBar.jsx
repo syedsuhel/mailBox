0fe5974fcdbec6e32ff1a1c7fc95175b26c54bc2
Syed Suhel M <syedsuhel2624@gmail.com>
2023-02-21 12:13:57 +0530

    used navigation

---
+++ components/AppBar.jsx
@@ -0,0 +1,21 @@
+import { View } from "react-native";
+
+
+
+const AppBar = () => {
+
+    return ( 
+        <View style={Styles.header}>
+
+        </View>
+
+    );
+}
+
+const Styles = StyleSheet.create({
+    header:{
+        
+    }
+})
+ 
+export default AppBar;
